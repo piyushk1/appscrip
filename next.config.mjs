51faa3bf44746c8cf42417afb83c18a79f414eb8
Piyush Kukade <piyushkukade1@gmail.com>
2024-10-15 13:06:13 +0530

    Appscrip

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
     images: {
-        domains: ['fakestoreapi.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'fakestoreapi.com',
+                port: '', 
+                pathname: '/**', 
+            },
+        ],
     },
 };
 
